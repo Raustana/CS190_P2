51fd65c6b1c7d7cdb47c049b4002fba07593bffd
Raustana <raustana@gmail.com>
2017-05-09 14:03:02 -0700

    Wwise integration. Soundbanks work now. Sounds added.
    Garage door and Engine off sounds in!

---
+++ CS190_Returning-Home/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID COMPUTER_TYPING = 2647449092U;
+        static const AkUniqueID COUCH_SIT = 1706561684U;
+        static const AkUniqueID DOOR_OPEN = 535830432U;
+        static const AkUniqueID ENGINE_OFF = 998053453U;
+        static const AkUniqueID FRIDGE_OPEN = 2916123785U;
+        static const AkUniqueID GARAGE_CLOSE = 2440369719U;
+        static const AkUniqueID GLASS_DOOR = 291040174U;
+        static const AkUniqueID TELEVISION_ON = 543766455U;
+        static const AkUniqueID TOILET = 580245626U;
+        static const AkUniqueID WATER_RUN = 2594811980U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
