1d4827f05a1a6901b3bc5fed9efeeeec62e6a64f
Raustana <raustana@gmail.com>
2017-05-10 23:20:18 -0700

    Exectuable.

---
+++ CS190_Returning-Home/EXECUTABLE/Returning Home_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID _FOOT_PLAYER = 3769161284U;
+        static const AkUniqueID _PICKUPS = 981037167U;
+        static const AkUniqueID BIRD_CHIRP = 4011507809U;
+        static const AkUniqueID COMPUTER_TYPING = 2647449092U;
+        static const AkUniqueID COUCH_SIT = 1706561684U;
+        static const AkUniqueID DOOR_CLOSE = 128625444U;
+        static const AkUniqueID DOOR_OPEN = 535830432U;
+        static const AkUniqueID ENGINE_OFF = 998053453U;
+        static const AkUniqueID FRIDGE_CLOSE = 2578282063U;
+        static const AkUniqueID FRIDGE_OPEN = 2916123785U;
+        static const AkUniqueID GARAGE_CLOSE = 2440369719U;
+        static const AkUniqueID GLASS_CLOSE = 633390694U;
+        static const AkUniqueID GLASS_DOOR = 291040174U;
+        static const AkUniqueID TELEVISION_ON = 543766455U;
+        static const AkUniqueID TOILET = 580245626U;
+        static const AkUniqueID WATER_RUN = 2594811980U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIALS
+        {
+            static const AkUniqueID GROUP = 4050929301U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID TILE = 2637588553U;
+            } // namespace SWITCH
+        } // namespace MATERIALS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PICKUPS = 488853820U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
